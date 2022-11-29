b49ea558b7255b1cc95cc3eaf05d92000c6aa96b
Alfin Kurniawan Aryanto <alfinkurniawan9727@gmail.com>
2022-11-29 15:15:13 +0700

    backup

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ALFINKA/golang-websocket
 
 go 1.19
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/novalagung/gubrak/v2 v2.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,62 @@
-package golang_web_socket
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"net/http"
+)
+
+type M mapp[string]interface{}
+
+const MESSAGE_NEW_USER = "New User"
+const MESSAGE_CHAT = "Chat"
+const MESSAGE_LEAVE = "Leave"
+
+var connections = make([]*WebSocketConnection,0)
+
+type SocketPayload struct {
+	Message string
+}
+
+type SocketResponse struct {
+	From string
+	Type string
+	Message string
+}
+
+type WebSocketConnection struct {
+	*websocket.Conn
+	Username string
+
+}
+
+func main(){
+	http.HandleFunc("/", func(writer http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			http.Error(w,"Cold not open requested file",http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w,"%s",content)
+	})
+
+	http.HandleFunc("/ws", func(writer http.ResponseWriter, r *http.Request) {
+	currentGorillaConn, err := websocket.Upgrade(w,r,w.Header(),1024,1024)
+	if err !=nil {
+		http.Error(w,"Could not open websocket connection",http.StatusBadRequestq)
+
+	}
+
+	username := r.URL.Query().Get("username")
+	currentConn := WebSocketConnection{Conn: currentGorillaConn, Username: username}
+	connections = append(connections,&currentConn)
+
+	go handleIO(&currentConn,connections)
+	})
+
+
+	fmt.Println("server starting at :8080")
+	http.ListenAndServe(":8080",nil)
+}
